12c11f1d18921f185f457fc23fa6d589a722f72e
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2023-11-03 14:01:11 +0700

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -38,34 +38,94 @@
 
 // visible dan invisible widgets
 
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+
+// main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         resizeToAvoidBottomInset: false,
+//         appBar: AppBar(
+//           title: Text("Columnn"),
+//         ),
+//         body: Column(
+//           // stack ganti column tanpa main dan cross dibawah
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.start,
+
+//           children: [
+//             Container(width: 200, height: 50, color: Colors.green),
+//             Container(width: 50, height: 50, color: Colors.blue),
+//             Container(width: 100, height: 50, color: Colors.amber),
+//             Container(width: 300, height: 50, color: Colors.red),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // List<Color> myColor = [Colors.red, Colors.green, Colors.blue, Colors.amber];
+
+  // List<Widget> myList = [
+  //   Container(height: 300, width: 300, color: Colors.red),
+  //   Container(height: 300, width: 300, color: Colors.green),
+  //   Container(height: 300, width: 300, color: Colors.blue),
+  //   Container(height: 300, width: 300, color: Colors.amber),
+  // ];
+
+  final List<Widget> myList = List.generate(
+      100,
+      (index) => Text(
+            "${index + 1}",
+            style: TextStyle(fontSize: 20 + double.parse(index.toString())),
+          ));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: Text("Columnn"),
+          title: Text("List View"),
         ),
-        body: Column(
-          // stack ganti column tanpa main dan cross dibawah
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        // body: ListView.builder(
+        //   itemCount: myColor.length,
+        //   itemBuilder: (context, index) {
+        //     return Container(height: 300, width: 300, color: myColor[index]);
+        //   },
+        //   // scrollDirection: Axis.horizontal,
+        //   // children: myList),
+        // ),
+        // body: ListView.separated(
+        body: ListView(children: myList),
+        // separatorBuilder: (context, index) {
+        //   // return Container(
+        //   //   height: 10,
+        //   // );
 
-          children: [
-            Container(width: 200, height: 50, color: Colors.green),
-            Container(width: 50, height: 50, color: Colors.blue),
-            Container(width: 100, height: 50, color: Colors.amber),
-            Container(width: 300, height: 50, color: Colors.red),
-          ],
-        ),
+        //   return Divider(
+        //     color: Colors.black,
+        //   );
+        // },
+        // itemCount: myColor.length,
+        // itemBuilder: (context, index) {
+        //   // return Container(height: 300, width: 300, color: myColor[index]);
+        //   return myList;
+        // },
+        // scrollDirection: Axis.horizontal,
+        // children: myList),
       ),
     );
   }
